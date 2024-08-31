c79ca39d3da584ecb8e24c2d033c83831f5c9009
Otacilio Jr. <81887187+jlj188@users.noreply.github.com>
2024-08-31 17:04:29 -0300

    Add files via upload

---
+++ slider.js
@@ -0,0 +1,32 @@
+
+
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 2,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+      "@0.00": {
+        slidesPerView: 1,
+        spaceBetween: 10,
+      },
+      "@0.75": {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      "@1.00": {
+        slidesPerView: 3,
+        spaceBetween: 30,
+      },
+      
+    },
+});
+
